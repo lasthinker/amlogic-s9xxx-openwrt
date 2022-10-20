bb5c3b0330158263bd1a785e234c86834d431178
Hiranika <h1r4n1k4@gmail.com>
2022-10-20 23:38:23 +0700

    Add the source name to the file name

--- router-config/openwrt-master/diy-part2.sh
+++ router-config/openwrt-master/diy-part2.sh
@@ -15,8 +15,8 @@ sed -i 's/luci-theme-bootstrap/luci-theme-material/g' feeds/luci/collections/luc
 sed -i 's/root::0:0:99999:7:::/root:$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.:0:0:99999:7:::/g' package/base-files/files/etc/shadow
 
 # Set etc/openwrt_release
-sed -i "s|DISTRIB_REVISION='.*'|DISTRIB_REVISION='$(date +%Y.%m.%d)'|g" package/base-files/files/etc/openwrt_release
-echo "DISTRIB_SOURCECODE='openwrt.master'" >>package/base-files/files/etc/openwrt_release
+sed -i "s|DISTRIB_REVISION='.*'|DISTRIB_REVISION='R$(date +%Y.%m.%d)'|g" package/base-files/files/etc/openwrt_release
+echo "DISTRIB_SOURCECODE='official'" >>package/base-files/files/etc/openwrt_release
 
 # Modify default IP（FROM 192.168.1.1 CHANGE TO 192.168.2.1）
 sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
