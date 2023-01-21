22f742edeae442e8ba84a5f5e7a56d41725dfa21
Hiranika <h1r4n1k4@gmail.com>
2023-01-21 23:16:31 +0700

    Optimize sample code

--- router-config/openwrt-master/diy-part1.sh
+++ router-config/openwrt-master/diy-part1.sh
@@ -6,13 +6,9 @@
 # Source code repository: https://github.com/openwrt/openwrt / Branch: master
 #========================================================================================================================
 
-# Uncomment a feed source
-# sed -i 's/#src-git helloworld/src-git helloworld/g' ./feeds.conf.default
-# sed -i 's/\"#src-git\"/\"src-git\"/g' feeds.conf.default
-
 # Add a feed source
 # sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 
 # other
-rm -rf package/lean/{ddns-scripts_aliyun,ddns-scripts_dnspod}
+# rm -rf package/utils/{ucode,fbtest}
 
